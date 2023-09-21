505656d5bf455f91c5502d71c197c2bc53280984
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-09-21 15:05:39 +0300

    Initial commit

---
+++ src/test/java/com/example/job4j_dreamjob/Job4jDreamjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job4j_dreamjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jDreamjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
